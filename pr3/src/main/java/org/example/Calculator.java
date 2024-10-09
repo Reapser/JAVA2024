fbe88a02e51edbb6846229591b1ed992013c2ae1
Reapser <22102004artem@gmail.com>
2024-10-09 21:46:16 +0300

    pr3

---
+++ pr3/src/main/java/org/example/Calculator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+class Calculator {
+
+    // Методи
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Ділення на нуль неможливе.");
+        }
+        return a / b;
+    }
+    public double sqrt(double a) throws InvalidInputException {
+        if (a < 0) {
+            throw new InvalidInputException("Неможливо обчислити квадратний корінь з від'ємного числа.");
+        }
+        return Math.sqrt(a);
+    }
+}
+
