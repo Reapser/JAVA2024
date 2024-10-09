b674569ce7470aefd164cf1654769d69127a103c
Reapser <22102004artem@gmail.com>
2024-10-09 21:44:26 +0300

    пр2

---
+++ 2Pr/src/main/java/org/example/TransactionAnalyzer.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TransactionAnalyzer {
+    private final List<Transaction> transactions;
+    private DateTimeFormatter dateFormatter;
+
+    public TransactionAnalyzer(List<Transaction> transactions) {
+        this.transactions = transactions;
+        this.dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    // Метод для розрахунку загального балансу
+    public double calculateTotalBalance() {
+        double balance = 0;
+        for (Transaction transaction : transactions) {
+            balance += transaction.getAmount();
+        }
+        return balance;
+    }
+
+    // Метод для підрахунку транзакцій за конкретний місяць і рік
+    public int countTransactionsByMonth(String monthYear) {
+        int count = 0;
+        for (Transaction transaction : transactions) {
+            LocalDate date = LocalDate.parse(transaction.getDate(), dateFormatter);
+            String transactionMonthYear = date.format(DateTimeFormatter.ofPattern("MM-yyyy"));
+            if (transactionMonthYear.equals(monthYear)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Метод для пошуку 10 найбільших витрат
+    public List<Transaction> findTopExpenses() {
+        return transactions.stream()
+                .filter(t -> t.getAmount() < 0)
+                .sorted(Comparator.comparing(Transaction::getAmount))
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+}
+
